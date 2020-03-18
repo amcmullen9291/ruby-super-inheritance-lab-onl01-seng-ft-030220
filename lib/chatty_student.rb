69eb427ea365e687a7f0298cd431a62e28e7b8f7
anthony mcmullen <amcmullen9291@gmail.com>
2020-03-18 18:28:48 +0000

    Automatically backed up by Learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -8,11 +8,10 @@ class ChattyStudent < Student
   def raise_hand
     super 
     i=1
-    while 1<= 10 do 
+    while i<= 10 do 
     puts "Pick me!"
     i+=1
-  end
-  end 
+    end
   end 
 
 end 
